beb1be29f82a34fd8afc070f61925d9e67a5242e
yahyo78 <yahyojanhasanov2007@gmail.com>
2026-05-07 13:33:51 +0500

    alias create

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+ 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <AppSidebar /> */}
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to React</h1>
+        </header>
+        <main>
+          <p>This is a simple React app.</p>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
